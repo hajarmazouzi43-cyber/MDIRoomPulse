cd2116bbd1fff74adc6304d25a2be8e55ed03217
Hajar MAZOUZI <hajarmazouzi43@gmail.com>
2026-07-05 15:36:22 +0100

    Fix QR Code error

--- app/components/rooms/RoomQRCode.tsx
+++ app/components/rooms/RoomQRCode.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import QRCode from 'qrcode.react'
+import { QRCodeCanvas } from 'qrcode.react'
 
 export default function RoomQRCode({ roomId, roomName }: { roomId: string, roomName: string }) {
   const url = `${window.location.origin}/rooms/${roomId}`
 
   return (
     <div className="flex flex-col items-center p-4 bg-white rounded-lg shadow">
-      <QRCode value={url} size={120} />
+      <QRCodeCanvas value={url} size={120} />
       <p className="text-xs text-gray-500 mt-2">Scan to view {roomName}</p>
     </div>
   )
